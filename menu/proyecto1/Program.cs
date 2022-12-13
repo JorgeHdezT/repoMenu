1241e39c35b101b497365a11673b95d866cca3f0
KeliamGlezz <keliamglezz@gmail.com>
2022-12-13 13:20:07 +0000

    V.0.0.0
    Programa Inicial

---
+++ menu/proyecto1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Crear un menu que si introduces 1 suma, si pones 0 resta*/
+        }
+    }
+}
